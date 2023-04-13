c4d48b8876c013de6fa975f744f1412b6520be24
Lecislav <lecislav@protonmail.com>
2023-04-13 20:04:38 +0200

    adding basic button and creating raw layout of website

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-
-
+const assetNameList = document.getElementById("assetName");
+fetch("https://api.llama.fi/protocols", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((result) => result.json())
+  .then((data) => {
+    data.find((ele) => {
+      const assetName = document.createElement("option");
+      assetName.setAttribute("value", ele.name);
+      assetNameList.appendChild(assetName);
+    });
+    console.log("finish");
+  });
