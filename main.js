75a43437f22f8a8bb8b443abc45a961d1b87281d
Lecislav <lecislav@protonmail.com>
2023-04-14 12:32:59 +0200

    added new layout elements and modal window

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const modal = document.getElementById("myModal");
+const overlay = document.querySelector(".modal");
+const buttonSignIn = document.getElementById("buttonSignIn");
+const exemple = document.getElementById("exemple");
+console.log(buttonSignIn);
+
+buttonSignIn.addEventListener("click", (event) => {
+  console.log('lol');
+  console.log(modal.style.display);
+  modal.style.display ="block";
+  overlay.style.display = "block";
+
+});
+
+overlay.addEventListener('click',(event)=>{
+  modal.style.display = 'none';
+  overlay.style.display = "none";
+  console.log("lol");
+});
+
 const assetNameList = document.getElementById("assetName");
 fetch("https://api.llama.fi/protocols", {
   method: "GET",
